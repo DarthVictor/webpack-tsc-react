8a1fe62f22c4b7b5582de5bb23a6beb7afed0da0
Victor Follet <follet.victor@gmail.com>
2017-01-08 02:33:16 +0300

    Server API with Express and TypeScript

--- src/modules/get-post.js
+++ src/modules/get-post.js
@@ -1,5 +1,5 @@
 export default async function getPost(postId){
-    const response = await window.fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`) 
+    const response = await window.fetch(`/api/posts/${postId}`) 
     const json = await response.json()
     return json
 }
--- src/modules/get-user.ts
+++ src/modules/get-user.ts
@@ -1,6 +1,6 @@
 declare var module: { hot: any };
 export default async function getUser(userId: number){
-    const response = await window.fetch(`https://jsonplaceholder.typicode.com/users/${userId}`) 
+    const response = await window.fetch(`/api/users/${userId}`) 
     const json = await response.json()
     return json
 }
